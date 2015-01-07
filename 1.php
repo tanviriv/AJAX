995cfad352c4c455c083578fe910645e7afc1a33
tanviriv <tanviriv@gmail.com>
2015-01-07 11:38:15 +0600

    ajax

---
+++ 1.php
@@ -0,0 +1,21 @@
+<html>
+<head></head>
+<body>
+<div
+
+id="myAJAXContent">
+HELLO
+
+</div>
+
+
+<script src="jquery-1.11.2.js"></script>
+<script>
+
+$(document).ready(function(){
+$("#myAJAXContent").html("HELLO FROM JS");
+});
+
+</script>
+</body>
+</html>
